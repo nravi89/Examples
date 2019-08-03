9dc22bccf42d9a340d4ec5dc609ed0d1c4dfb4f6
nravi89 <n.ravi89@gmail.com>
2019-08-03 11:29:38 +0530

    selenium test

---
+++ seleniumTest/src/main/java/basics/T1BrowserDriver.java
@@ -0,0 +1,24 @@
+package basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class T1BrowserDriver {
+	
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver", "C:\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		
+		/*System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();*/
+		
+		String baseUrl = "http://www.google.com";
+		
+		driver.get(baseUrl);
+		
+		System.out.println(driver.getTitle());
+		
+		driver.close();
+	}
+
+}
